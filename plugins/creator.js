a21b543155f899b058650fe3822f9f7c442575df
rakzzz10 <81899240+rakzzz10@users.noreply.github.com>
2021-04-05 17:59:49 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 let handler = function (m) {
   // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
-  this.sendContact(m.chat, '6288235435804', 'My Owner BOT', m)
+  this.sendContact(m.chat, '6283161362292', 'My Owner BOT', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
